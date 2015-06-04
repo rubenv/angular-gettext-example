5f4b9fc46420e9ecbd6263a60a6d208004efc9d7
Ruben Vermeersch <ruben@rocketeer.be>
2015-06-04 09:08:39 +0200

    Bump to newer versions

--- js/translations.js
+++ js/translations.js
@@ -1,4 +1,5 @@
-angular.module("gettext").run(['gettextCatalog', function (gettextCatalog) {
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
     gettextCatalog.setStrings('nl', {"Mark all as complete":"Markeer als afgewerkt","{{$count}} item left":["{{$count}} item te gaan","{{$count}} items te gaan"],"All":"Alle","Active":"Actief","Completed":"Afgewerkt","Clear completed ({{completedCount}})":"Wis afgewerkte taken ({{completedCount}})","Double-click to edit a todo":"Dubbelklik om een todo te bewerken","What needs to be done?":"Wat moet gedaan worden?"});
-
+/* jshint +W100 */
 }]);
